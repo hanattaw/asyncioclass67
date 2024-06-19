4ea4608252384a7206f8981b71a0086f94b0cc54
Benzza9899 <6510301017@cdti.ac.th>
2024-06-19 11:34:01 +0700

    submit 01 assighment

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,17 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+# a custom function that blocks for a moment
+def task():
+
+    sleep(5)
+
+    print(f'{ctime()} This is from another thread')
+
+thread = Thread(target=task)
+
+thread.start()
+
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
