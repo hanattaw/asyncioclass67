da8cfd6737601175e2f0c9016ec99a3f055f4f32
Benzza9899 <6510301017@cdti.ac.th>
2024-06-19 13:38:13 +0700

    submit 01 assighment

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,17 @@
 # running a function with arguments in another thread
+from time import sleep, ctime
+from threading import Thread
+
+#a cutom function that blocks for a moment
+def task(sleep_time, message):
+
+    sleep(sleep_time)
+
+    print(f'{ctime()} {message}')
+
+thread = Thread(target=task, args=(1.5, 'New message from anorther thread'))
+
+thread.start()
+
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,24 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+#a cutom function that blocks for a moment
+class CustomThread(Thread):
+
+    def run(self):
+
+        sleep(1)
+
+        print(f'{ctime()} This is coming from anther thread')
+
+        self.value = 99
+
+thread = CustomThread()
+
+thread.start()
+
+print(f'{ctime()} Waiting for the thread to finish')
+thread.join()
+
+value = thread.value
+print(f'{ctime()} Got: {value}')
