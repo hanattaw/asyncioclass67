2ab3fb3d70eb47b1e27107c25986f0b766f7f6b3
thanabodin19 <thanabodin.kea@gmail.com>
2024-06-21 10:00:28 +0700

    add thread1

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -10,9 +10,9 @@ def task():
     print(f'{ctime()} This is from another thread')
 
 # create a thread
-thread = Thread(target=task)
+thraed = Thread(target=task)
 # run the thread
-thread.start()
+thraed.start()
 # wait for the thread to finish
 print(f'{ctime()} Waiting for the thread...')
-thread.join()
+thraed.join()
