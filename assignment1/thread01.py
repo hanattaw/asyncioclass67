d8b50c1218545cf04b6365d6b3008891e324a081
sirapaop <sirapaop2545@gmail.com>
2024-06-21 10:01:52 +0700

    done thread01

--- assignment1/thread01.py
+++ assignment1/thread01.py
@@ -1,1 +1,12 @@
 # running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+def task():
+    sleep(1)
+    print(f'{ctime()} This is from another thread')
+
+thread = Thread(target = task)
+thread.start()
+print(f'{ctime()} waiting for the thread...')
+thread.join()
---
+++ hello.py
@@ -0,0 +1,2 @@
+msg = "Roll a dice"
+print(msg)
