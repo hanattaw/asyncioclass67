ebca31952c05f480775a8816d3f43e35ac8f4e3d
sariszoom <sariszoom@gmail.com>
2024-06-21 10:53:32 +0700

    4-9

--- assignment1/thread06.py
+++ assignment1/thread06.py
@@ -1,1 +1,27 @@
 # Working With Many Threads
+
+import logging
+import threading
+import time
+
+def thread_function (name):
+    logging.info("Thread %s: starting", name) 
+    time.sleep(2)
+    logging.info("Thread %s: finishing", name)
+
+if __name__ == "__main__":
+    format = "%(asvtime)s: %(message)s"
+    logging.basicConfig(format=format, level=logging.INFO,
+                        datefmt="%H:%M:%S")
+    
+    threads = list()
+    for index in range(3):
+        logging.info("Main : creating and start thread %d",index)
+        x = threading.Thread(target=thread_function, args=(index,))
+        threads.append(x)
+        x.start()
+
+    for index, thread in enumerate(threads):
+        logging.info("Main  : before joining thread %d", index)
+        thread.join()
+        logging.info("Main  : thread %d done",index)
