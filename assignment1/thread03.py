be0728b14f56abeeb31e7ddef8294bb0ab0a1e91
sirapaop <sirapaop2545@gmail.com>
2024-06-21 10:22:43 +0700

    done thread03

--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,20 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+# custom thread class
+class CustomThread(Thread):
+    # override the run function
+    def run(self):
+        sleep(1)
+        print(f'{ctime()} this is coming from another thread')
+        # store return value
+        self.value = 99
+
+thread = CustomThread()
+thread.start()
+print(f'{ctime()} waiting for the thread to finish')
+thread.join()
+# get the value returned from run
+value = thread.value
+print(f'{ctime()} Go: {value}')
