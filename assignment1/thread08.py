ed7643e75580e546ed1862aa65ad460bcda3a1a2
gift <karinakornccn30@gmail.com>
2024-06-21 11:23:47 +0700

    new

--- assignment1/thread08.py
+++ assignment1/thread08.py
@@ -1,29 +1,28 @@
 # Race Conditions
-# Race Conditions
 import concurrent.futures
 import logging
 import time
 
 class FakeDatabase:
-        def __init__(self):
-            self.value = 0
+    def __init__(self):
+        self.value = 0
         
-        def update(self, name):
-            logging.info("Thread &s: starting update", name)
-            local_copy = self.value
-            local_copy += 1
-            time.sleep(0.1)
-            self.value = local_copy
-            logging. info("Thread &s: finishing update", name)
+    def update(self, name):
+        logging.info("Thread %s: starting update", name)
+        local_copy = self.value
+        local_copy += 1
+        time.sleep(0.1)
+        self.value = local_copy
+        logging.info("Thread %s: finishing update", name)
 
 
 if __name__ == "__main__":
     format = "%(asctime)s: %(message)s"
-    logging.basicConfig(format=format,level=logging.INFO, datefmt="%H:%m:%S")
+    logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%m:%S")
 
     database = FakeDatabase()
-    logging.info("Testing update. Starting value is &d,", database.value)
-    with concurrent.futures.ThreadPoolEecutor(max_workers=2) as executor:
+    logging.info("Testing update. Starting value is %d,", database.value)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=2) as executor:
         for index in range(2):
             executor.submit(database.update, index)
     logging. info("Testing update. Ending value is %d.", database.value)
