5cc00ce75b1d6c51e9f7849e8820f68f7f06f5c9
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2024-06-21 10:16:18 +0700

    thread03 done

--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,20 @@
 # extending the Thread class
+from time import sleep, ctime
+from threading import Thread
+
+# custom thread class
+class CustomThread(Thread):
+    # override the run function
+    def run(self):
+        # block for a moment
+        sleep(1)
+        #display message
+        print(f'{ctime()} This is coming from another thread')
+
+# create thread
+thread = CustomThread()
+#run the thread
+thread.start()
+#wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
