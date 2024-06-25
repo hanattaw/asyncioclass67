baadaa9ccba90a57e42b0422aa6e59dee7f2b62b
Wattana Hinchaisri <hanattaw@gmail.com>
2024-06-25 21:51:09 +0700

    ส่งไฟล์ thread02 เพื่อทดแทนอันเดิมที่ทำมา

--- assignment1/thread02.py
+++ assignment1/thread02.py
@@ -1,1 +1,18 @@
 # running a function with arguments in another thread
+from time import sleep, ctime
+from threading import Thread
+
+# a custom function that blocks for a moment
+def task(sleep_time, message):
+    # block for a moment
+    sleep(sleep_time)
+    # display a message
+    print(f'{ctime()} {message}')
+
+# create a thread
+thread = Thread(target=task, args=(1.5, 'New message from another thread'))
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread...')
+thread.join()
