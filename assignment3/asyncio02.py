a1a48b804cdf0152ed446d7cafb5b96b6e323185
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2024-06-28 11:20:01 +0700

    assgnment2-3

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,13 @@
 # example of running a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
 
+# create the coroutine
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+# check the type of the coroutine
+asyncio.run(main())
