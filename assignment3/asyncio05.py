09a84e54c45f77b32856912300545e20109b99f3
sirapaop <sirapaop2545@gmail.com>
2024-06-28 11:22:12 +0700

    finish assignment3

--- assignment3/asyncio05.py
+++ assignment3/asyncio05.py
@@ -1,1 +1,24 @@
 # When a coroutine is wrapped into a Task with functions like asyncio.create_task() the coroutine is automatically scheduled to run soon
+
+import asyncio
+import aiohttp
+from time import ctime
+
+async def get_api(client, url):
+    async with client.get(url) as response:
+        return await response.json()
+    
+async def fetch_hacker_news():
+    async with aiohttp.ClientSession() as session:
+        url1 = f"https://hacker-news.firebaseio.com/v0/item/24661271.json?print=pretty"
+        task1 = asyncio.create_task(get_api(session, url1))
+
+        url2 = f"https://hacker-news.firebaseio.com/v0/item/24659683.json?print=pretty"
+        task2 = asyncio.create_task(get_api(session, url2))
+
+        news1 = await task1 # wait till task 1 is complete
+        news2 = await task2 # wait till task 2 is complete
+        print(f"{ctime()} {news1['title']}")
+        print(f"{ctime()} {news2['title']}")
+
+asyncio.run(fetch_hacker_news())
