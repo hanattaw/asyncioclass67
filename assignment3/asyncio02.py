5a367b0cee43b7f7010aea1c67ebc5ecfcf34f12
Kantinan Boonyingsathit <kaseam008@gmail.com>
2024-06-28 11:15:45 +0700

    done assignment2 and 3

--- assignment3/asyncio02.py
+++ assignment3/asyncio02.py
@@ -1,2 +1,14 @@
 # example of running a coroutine
+import asyncio
+# define a coroutine
+async def custom_coro():
+    # await another coroutine
+    await asyncio.sleep(1)
 
+# main coroutine
+async def main():
+    # execute my custom coroutine
+    await custom_coro()
+
+# start the coroutine program
+asyncio.run(main())
