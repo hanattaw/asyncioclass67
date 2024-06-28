e2e886c48cf6ae9c20f90148cfe515a4935fc7c6
Thanabodin19 <thanabodin.kea@gmail.com>
2024-06-28 11:08:40 +0700

    done assignment3

--- assignment3/asyncio04.py
+++ assignment3/asyncio04.py
@@ -1,1 +1,26 @@
 # Starting task
+from time import ctime
+import asyncio
+
+async def wash(basket):
+    print(f'{ctime()}   : Washing Machine ({basket}): Put thw coin')
+    print(f'{ctime()}   : Washing Machine ({basket}): Start washing')
+    await asyncio.sleep(5)
+    print(f'{ctime()}   : Washing Machine ({basket}): Finished washing')
+    return f'{ctime()}  : {basket} is completed'
+
+async def main():
+    #create coroutine
+    coro = wash('Basket A')
+    print(f'{ctime()}   : {coro}')
+    print(f'{ctime()}   : {type(coro)}')
+    #create task
+    task = asyncio.create_task(coro)
+    print(f'{ctime()}   : {task}')
+    print(f'{ctime()}   : {type(task)}')
+    #run the task 
+    result = await task
+    print(f'{ctime()}   : {result}')
+
+if __name__ == "__main__":
+    asyncio.run(main())
