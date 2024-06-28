d08360010b96fd2094a87b855ab2177effee3e30
gift <karinakornccn30@gmail.com>
2024-06-28 11:33:50 +0700

    new

--- assignment2/process02.py
+++ assignment2/process02.py
@@ -1,2 +1,63 @@
 # Multiprocessing 2 kitkens, 2 cooker, 2 dishes
 # share resources
+# Multiprocessing 2 kitkens, 2 cooker, 2 dishes
+
+# Share resources
+
+import os
+import multiprocessing
+from time import sleep, ctime, time
+
+# Basket of sharing
+
+class Basket:
+    def __init__(self):
+        self.eggs = 50
+    def use_eggs(self, index):
+        print(f"{ctime()} Kitchen-{index} : Chef-{index} has lock with eggs remaining (self.eggs)")
+        self.eggs -= 2
+        print(f"{ctime()} Kitchen-{index} : Chef-{index} has released lock with eggs remaining (self.eggs)")
+
+# Chef cooking
+
+def cooking(index, basket):
+    #chef use egs fos cooking
+    basket.use_eggs(index)
+    sleep(2)
+
+
+# Kitchen cooking
+
+def kitchen(index, share_eggs):
+    print(f"{ctime()} Kitchen-{index} : Begin cooking... PID (os.getpid())")
+    cooking_time = time()
+    cooking(index, share_eggs)
+    duration = time() - cooking_time
+    print(f"{ctime()} Kitchen-{index} : Cooking done in {duration:0.2f} seconds!)")
+    
+
+if __name__ == "__main__":
+
+    # Begin of main thread
+    print(f"{ctime()} Main : Start Cooking... PID (os.getpid())")
+    start_time = time()
+
+    basket = Basket()
+
+    # Multi processes
+    kitchens = list()
+    for index in range(2):
+        p = multiprocessing.Process(target=kitchen, args=(index, basket))
+        kitchens.append(p)
+        # Starting processes
+        p.start()
+    
+
+    # Wait until processes are finished
+    for index, p in enumerate(kitchens):
+        p.join()
+
+    # End of main thread
+    print(f"{ctime()} Main : Basket eggs remaining (basket.eggs)")
+    duration = time() - start_time
+    print(f"{ctime()} Main : Finished Cooking duration in {duration:0.2f} seconds)")
--- assignment2/thread01.py
+++ assignment2/thread01.py
@@ -1,1 +1,25 @@
 # Thread version of cooking 1 kitchen 1 chefs 1 dishes
+# Thread version of cooking 1 kitchen 1 chefs 1 dishes
+
+from time import time, ctime, sleep
+from threading import Thread
+
+def cooking(index):
+    start_time = time()
+    print(f'{ctime()} Kitchen-{index} : Begin cooking...')
+    sleep(2)
+    duration = time() - start_time
+    print(f'{ctime()} Kitchen-{index} : Cooking done in {duration:.2f} seconds!')
+
+if __name__ == "__main__":
+    print(f'{ctime()} Main : Starting cook.')
+    start_time = time()
+
+    # Thread cooking
+    index = 1
+    c1 = Thread(target=cooking, args=(index,))
+    c1.start()
+    c1.join()
+
+    duration = time() - start_time
+    print(f"{ctime()} Main : Finished Cooking duration in {duration:.2f} seconds")
