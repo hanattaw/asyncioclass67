baa455b77abe394023ac2e1ee9b356f536fe227e
Theelawee <ttrrvv.pp@gmail.com>
2024-07-03 13:26:19 +0700

    complete

--- assignment4/asyncio01.py
+++ assignment4/asyncio01.py
@@ -1,1 +1,14 @@
 # example of getting the current task from the main coroutine
+import asyncio
+
+#define a main corotine
+async def main():
+    #report a massage
+    print("main coroutine started")
+    #get the current task
+    task = asyncio.current_task()
+    #report this detils
+    print(task)
+
+#start the aysncio program
+asyncio.run(main())
--- assignment4/asyncio03.py
+++ assignment4/asyncio03.py
@@ -1,1 +1,23 @@
 # example of gather for many coroutines in a list
+import asyncio
+
+# coroutine used for a task
+async def task_corou(value):
+    # report a massage
+    print(f"Task {value} executing")
+    # sleep for a moment
+    await asyncio.sleep(1)
+
+# coroutine used for the entry point
+async def main():
+    # report a massage
+    print("main Starting")
+    # create many coroutines
+    coros = [task_corou(i) for i in range(10)]
+    #run the tasks
+    await asyncio.gather(*coros)
+    #report a massage
+    print("main done")
+
+# start the asyncio program
+asyncio.run(main())
