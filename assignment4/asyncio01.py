ce6172f3f10213fc4deeb6b66a1cd38c20da228e
flukepanu <6510301025@cdti.ac.th>
2024-07-03 14:38:40 +0700

    submit_assignment4

--- assignment4/asyncio01.py
+++ assignment4/asyncio01.py
@@ -1,1 +1,14 @@
 # example of getting the current task from the main coroutine
+import asyncio
+
+# define a mian coroutine
+async def main():
+    # report a message
+    print('main coroutine started')
+    # get teh current task
+    task = asyncio.current_task()
+    # report its details
+    print(task)
+
+# started the asyncio program
+asyncio.run(main())
--- assignment4/asyncio02.py
+++ assignment4/asyncio02.py
@@ -1,1 +1,29 @@
 # example of starting many tasks and getting access to all tasks
+import asyncio
+
+# coroutine for a task
+async def task_coroutine(value):
+    # report a message
+    print(f'task {value} is running')
+    # block for a moment
+    await asyncio.sleep(1)
+
+# definea main coroutine
+async def main():
+    # report a meassage
+    print('main coroutine started')
+    # start many tasks
+    started_tasks = [asyncio.create_task(task_coroutine(i)) for i in range(10)]
+    #allow some of the tasks time to start
+    await asyncio.sleep(0.1)
+    # get all tasks
+    tasks = asyncio.all_tasks()
+    # report all tasks
+    for task in tasks:
+        print(f'> {task.get_name()}, {task.get_coro()}')
+    # wait for all tasks to complete
+    for task in started_tasks:
+        await task
+
+# started the asyncio program
+asyncio.run(main())
