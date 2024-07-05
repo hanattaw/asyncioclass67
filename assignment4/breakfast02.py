46676c06d3c44a9b9e2d2c6896b60230b1dab9d9
sirapaop <sirapaop2545@gmail.com>
2024-07-05 11:12:33 +0700

    finish assignment4

--- assignment4/breakfast02.py
+++ assignment4/breakfast02.py
@@ -1,3 +1,26 @@
 # Concurrently breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(): # 1
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5) # 2 : pause, another tasks can be run
+    print("coffee: ready")
+
+async def fry_eggs(): # 1
+    print("eggs: prepare ingridients")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(5) # 2 : pause, another tasks can be run
+    print("eggs: ready")
+
+async def main():
+    start = time()
+    await make_coffee() # run task with await
+    await fry_eggs()
+    print(f"breakfast is ready in {time()-start} min")
+
+
+asyncio.run(main()) # run top level function concurrently
--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -1,3 +1,24 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(): # 1
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5) # 2 : pause, another tasks can be run
+    print("coffee: ready")
+
+async def fry_eggs(): # 1
+    print("eggs: prepare ingridients")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3) # 2 : pause, another tasks can be run
+    print("eggs: ready")
+
+async def main():
+    start = time()
+    await asyncio.gather(make_coffee(), fry_eggs())
+    print(f"breakfast is ready in {time()-start} seconds")
+
+asyncio.run(main()) 
