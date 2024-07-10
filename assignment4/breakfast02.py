2d5ba6b07d00ac51271886e4db90aee7f0559a84
Benzza9899 <6510301017@cdti.ac.th>
2024-07-10 10:09:56 +0700

    submit 04 assignment

--- assignment4/breakfast02.py
+++ assignment4/breakfast02.py
@@ -1,3 +1,25 @@
 # Concurrently breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(): #1
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    await asyncio.sleep(5) #2: pause, another tasks can be run
+    print("cofee: ready")
+
+async def fry_eggs(): #1
+    print("eggs: prepare ingridiants")
+    sleep(1)
+    print("eggs: frying...")
+    await asyncio.sleep(3)#2: pause, another tasks can be run
+    print("eggs: ready")
+
+async def main(): #1
+    start = time()
+    await make_coffee()
+    await fry_eggs()
+    print(f"breakfast is ready in {time()-start} min")
+
+asyncio.run(main()) #run top-level function concurrently
--- assignment4/breakfast03.py
+++ assignment4/breakfast03.py
@@ -1,3 +1,20 @@
 # Asynchronous breakfast
 import asyncio
 from time import sleep, time
+
+async def make_coffee(): #1
+    print("coffee: prepare ingridients")
+    sleep(1)
+    print("coffee: waiting...")
+    print("eggs: prepare ingridiants")
+    print("eggs: frying...")
+    await asyncio.sleep(5) #2: pause, another tasks can be run
+    print("cofee: ready")
+    print("eggs: ready")
+
+async def main(): #1
+    start = time()
+    await make_coffee()
+    print(f"breakfast is ready in {time()-start} min")
+
+asyncio.run(main()) #run top-level function concurrently
