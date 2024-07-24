f1ba82e3f85f1f4a0a211de7b7a470d77662d76c
flukepanu <6510301025@cdti.ac.th>
2024-07-24 22:37:37 +0700

    Submit assignment07

--- assignment1/thread03.py
+++ assignment1/thread03.py
@@ -1,1 +1,20 @@
 # extending the Thread class
+# running a function in another thread
+from time import sleep, ctime
+from threading import Thread
+
+# a custom function that blocks for a moment
+class CustonThread(Thread):
+    def run(self):
+        #block for a moment
+        sleep(1)
+        #display a message
+        print(f'{ctime()} This is coming from another thread')
+
+# create a thread
+thread = CustonThread()
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread to finish')
+thread.join()
--- assignment1/thread04.py
+++ assignment1/thread04.py
@@ -1,1 +1,24 @@
 # extending the Thread class and return values
+from time import sleep, ctime
+from threading import Thread
+
+# a custom function that blocks for a moment
+class CustonThread(Thread):
+    def run(self):
+        #block for a moment
+        sleep(1)
+        #display a message
+        print(f'{ctime()} This is coming from another thread')
+        #store return value
+        self.value = 99
+
+# create a thread
+thread = CustonThread()
+# run the thread
+thread.start()
+# wait for the thread to finish
+print(f'{ctime()} Waiting for the thread to finish')
+thread.join()
+# get the value returned from run
+value = thread.value
+print(f'{ctime()} Got: {value}')
