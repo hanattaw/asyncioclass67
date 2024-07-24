035809122eac3d31e1ebcd6785a9c7e5adb8a4fd
flukepanu <6510301025@cdti.ac.th>
2024-07-24 22:47:19 +0700

    Submit assignment07

--- assignment07/chess02.py
+++ assignment07/chess02.py
@@ -1,32 +1,35 @@
-import aiofiles
+import time
 import asyncio
-import json
-from pathlib import Path
 
-pokemonapi_directory = './assignment07/pokemon/pokemonapi'
-pokemonmove_directory = './assignment07/pokemon/pokemonmove'
+my_compute_time = 0.1  # Judit's thinking time in seconds
+opponent_compute_time = 0.5  # Opponent's thinking time in seconds
+opponents = 24  # Number of opponents
+move_pairs = 30  # Number of move pairs
 
-async def process_file(path):
-    # Read the contents of the json file.
-    async with aiofiles.open(path, mode='r') as f:
-        contents = await f.read()
-
-    # Load it into a dictionary and create a list of moves.
-    pokemon = json.loads(contents)
-    name = pokemon['name']
-    moves = [move['move']['name'] for move in pokemon['moves']]
-
-    # Open a new file to write the list of moves into.
-    async with aiofiles.open(f'{pokemonmove_directory}/{name}_move.txt', mode='w') as f:
-        await f.write('\n'.join(moves))
+async def game(x):
+    # Loops 30 times to simulate both players making a move
+    board_start_time = time.perf_counter()
+    for i in range(move_pairs):
+        time.sleep(my_compute_time)
+        print(f"BOARD-{x+1} ({i+1}) Judit made a move.")
+        
+        await asyncio.sleep(opponent_compute_time)
+        print(f"BOARD-{x+1} ({i+1}) Opponent made move.")
+        
+    print(f"BOARD-{x+1} >>>>>>>>>>>>>>> Finished move in {round(time.perf_counter() - board_start_time)} secs\n")
+    return round(time.perf_counter() - board_start_time)
 
 async def main():
-    pathlist = Path(pokemonapi_directory).glob('*.json')
+    start_time = time.perf_counter()
+    board_time = 0
+    # Create a list of game tasks
+    tasks = [game(board) for board in range(opponents)]
+    # Wait for all games to complete
+    results = await asyncio.gather(*tasks)
+    board_time = sum(results)
+    
+    print(f"Board exhibition finished in {board_time} secs.")
+    print(f"Finished in {round(time.perf_counter() - start_time)} secs.")
 
-    # Create tasks for each file.
-    tasks = [process_file(path) for path in pathlist]
-
-    # Wait for all tasks to complete.
-    await asyncio.gather(*tasks)
-
+# Run the main function
 asyncio.run(main())
