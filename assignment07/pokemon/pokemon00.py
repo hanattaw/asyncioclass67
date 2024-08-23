4b0dc7e97ecfb80d7e06865bddeef4b70c2e3e12
Jirachai <77221588+JaoTaohu@users.noreply.github.com>
2024-08-23 12:15:34 +0700

    finish assignment7

---
+++ assignment07/pokemon/pokemon00.py
@@ -0,0 +1,14 @@
+import aiofiles
+import asyncio
+import json
+
+pokemonapi_directory = './assignment07/pokemon/pokemonapi'
+pokemonmove_directory = './assignment07/pokemon/pokemonmove'
+
+async def main():
+    # read the contets of the json file
+    async with aiofiles.open(f'{pokemonapi_directory}/articuno.json', mode='r') as f:
+        contents = await f.read()
+    print(contents)
+
+asyncio.run(main())
