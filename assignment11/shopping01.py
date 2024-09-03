d60d7e252720989e2537efec142fabe0a01033fe
Wattana Hinchaisri <hanattaw@gmail.com>
2024-09-03 21:42:13 +0700

    Add assignment11 shopping

---
+++ assignment11/shopping01.py
@@ -0,0 +1,100 @@
+import time
+import asyncio
+from asyncio import Queue
+from random import randrange
+
+# we first implement the Customer and Product classes, 
+# representing customers and products that need to be checked out. 
+# The Product class has a checkout_time attribute, 
+# which represents the time required for checking out the product.
+class Product:
+    def __init__(self, product_name: str, checkout_time: float):
+
+
+
+class Customer:
+    def __init__(self, customer_id: int, products: list[Product]):
+
+
+
+# we implement a checkout_customer method that acts as a consumer.
+# As long as there is data in the queue, this method will continue to loop. 
+# During each iteration, it uses a get method to retrieve a Customer instance. 
+# 
+# If there is no data in the queue, it will wait. 
+# 
+# After retrieving a piece of data (in this case, a Customer instance), 
+# it iterates through the products attribute and uses asyncio.sleep to simulate the checkout process.
+# 
+# After finishing processing the data, 
+# we use queue.task_done() to tell the queue that the data has been successfully processed.
+async def checkout_customer(queue: Queue, cashier_number: int):
+    while not queue.empty():
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        queue.task_done()
+
+# we implement the generate_customer method as a factory method for producing customers.
+#
+# We first define a product series and the required checkout time for each product. 
+# Then, we place 0 to 4 products in each customer’s shopping cart.
+def generate_customer(customer_id: int) -> Customer:
+    all_products = [Product('beef', 1),
+                    Product('banana', .4),
+                    Product('sausage', .4),
+                    Product('diapers', .2)]
+    return Customer(customer_id, all_products)
+
+# we implement the customer_generation method as a producer. 
+# This method generates several customer instances regularly 
+# and puts them in the queue. If the queue is full, the put method will wait.
+async def customer_generation(queue: Queue, customers: int):
+    customer_count = 0
+    while True:
+
+
+
+
+
+
+
+
+        return customer_count
+
+# Finally, we use the main method to initialize the queue, 
+# producer, and consumer, and start all concurrent tasks.
+async def main():
+    
+    
+
+
+
+
+
+
+    
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+# +--------|------------|-------------|-----------------------|-------------------------    
+# Queue	   | Customer   | Cashier	  |  Time each Customer	  |  Time for all Customers
+# 2	       | 2	        | 2		      |                       |           
+# 2	       | 3	        | 2		      |                       |                                               		
+# 2	       | 4	        | 2		      |                       |           
+# 2	       | 10	        | 3		      |                       |           
+# 5	       | 10	        | 4			  |                       |               
+# 5	       | 20			|             |                       |  >= 8 s
+# +--------|------------|-------------|-----------------------|-------------------------    
